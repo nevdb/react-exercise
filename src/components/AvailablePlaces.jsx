1a06abf3de017775f4d10759bc0ffc8432285663
nevena <dnevena27@gmail.com>
2026-05-15 13:57:23 +0300

    add isLoading when fetching places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,14 +3,16 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   // Fetch available places from backed API
-
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3002/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -26,6 +28,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Loading places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
