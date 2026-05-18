6d670eb9edd7e454a2ba4626a0fc3c2c179f27d5
nevena <dnevena27@gmail.com>
2026-05-18 15:15:06 +0300

    fix issue with available places

--- src/http.js
+++ src/http.js
@@ -1,3 +1,14 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch("http://localhost:3000/places");
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error(resData.message || "Failed to fetch available places.");
+    }
+
+    return resData.places;
+}
+
 export async function fetchUserPlaces() {
     const response = await fetch("http://localhost:3000/user-places");
     const resData = await response.json();
