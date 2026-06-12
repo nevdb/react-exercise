585095390b9be92b67fe918e9d49730acc83c6c9
nevena <dnevena27@gmail.com>
2026-06-12 12:28:34 +0300

    add login form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Login from "../components/Login.jsx";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
       <p>Welcome to the home page.</p>
+
+      <Login />
     </main>
   );
 }
